6d46cf18dbcccdbe78da5332edae1fb4fce2d9c8
candy1919 <994256595@qq.com>
2017-06-30 18:18:31 +0800

    home page

--- build/mockData.js
+++ build/mockData.js
@@ -19,4 +19,31 @@ app.get('/home/banner', function (req, res) {
 
 	res.send( JSON.stringify(data, null, 4) );
 });
+app.get('/home/shoplist', function (req, res) {
+	var Random = Mock.Random,
+	    data = Mock.mock({
+	    	retcode: 1,
+	        retmsg: '查询成功',
+	        data: {
+	            'total': 20,//一共20页
+	            'records': 10,//每页10条记录
+	            'page': 1,
+	            'rows|10': [{
+	                'id|+1': 1,
+	                'sid|+1': 1,
+	                'remainmember': '@natural(100, 1000)',
+	                'joinedmember': '@natural(100, 1000)',
+	                'totalmember': '@natural(100, 1000)',
+	                'title': '@title(6, 15)',
+	                'cover|1': ['http://scimg.jb51.net/allimg/150819/14-150QZ9194K27.jpg',
+                        'http://pic15.photophoto.cn/20100615/0006019058815826_b.jpg',
+                        'http://img15.3lian.com/2016/h1/124/171.jpg',
+                        'http://pic15.photophoto.cn/20100615/0006019058815826_b.jpg',
+                        'http://pic16.photophoto.cn/20100727/0006019030380410_b.jpg'
+                       ]
+	            }]
+	        }
+	    })
+	    res.send( JSON.stringify(data, null, 4) );
+});
 module.exports = app;
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -16,17 +16,18 @@
 			<li>上新</li>
 			<li>价格<span class="up">▲</span><span class="down">▼</span></li>
 		</ul>
-		<div class="list-container">
-			<div class="item">
-				<div class="img-wrap">
-					<img >
-				</div>
-				
-				<p class="title">标题</p>
+		<div class="list-container" v-if="shoplist.retcode">
+			<div class="item" v-for="item in shoplist.data.rows" :key="item.sid">
+				<!-- <a href=""> -->
+					<div class="img-wrap">
+						<img :src="item.cover">
+					</div>
+					<p class="title">{{item.title}}</p>
+				<!-- </a> -->
 				<div class="msg">
 					<div class="state">
-						<p class="hit">开奖进度</p>
-						<p class="progress"><span></span></p>
+						<p class="hit">开奖进度{{calPercent (item.joinedmember,item.totalmember)}}%</p>
+						<p class="progress"><span :style=""></span></p>
 					</div>
 					<button class="red-btn">+清单</button>
 				</div>
@@ -38,7 +39,9 @@
 export default{
   data () {
     return {
-      banners: {}
+      banners: {},
+      shoplist: {},
+      page: 1
     }
   },
   created () {
@@ -46,6 +49,18 @@ export default{
       this.banners = response.body
     }, response => {
     })
+    this.getShopList()
+  },
+  methods: {
+    getShopList () {
+      this.$http.get('/home/shoplist', {params: {'page': 1}}).then(response => {
+        this.shoplist = response.body
+      }, response => {
+      })
+    },
+    calPercent (join, total) {
+      return ((join / total) * 100).toFixed(0)
+    }
   }
 }
 </script>
@@ -76,8 +91,8 @@ export default{
 			display: inline-block;
 			padding: 10px 10px 15px 10px;
 			width: 50%;
-			border-right: 1px solid black;
-			border-bottom: 1px solid black;
+			border-right: 1px solid #ddd;
+			border-bottom: 1px solid #ddd;
 			.img-wrap{
 				position: relative;
 				width: 100%;
@@ -88,22 +103,49 @@ export default{
 					left: 0;
 					right: 0;
 					bottom: 0;
+					width: 100%;
+					height: 100%;
 				}
+			}
+			.title{
+				position: relative;
+				line-height: 1.2em;
+				height: 2.4em;
+				overflow: hidden;
+				&:after{
+					position: absolute;
+					content: "...";
+					bottom: 0;
+					right: 0;
+				}
+				//-webkit-line-clamp:2;
 			}
 			.msg{
 				display: flex;
+			    margin-top: 3px;
+                font-size: 12px;
 				.state{
 					flex:1;
+					font-size: 0;
+					.hit{
+						font-size: 12px;
+					}
 					.progress{
+						margin-top: 3px;
 						width: 80%;
-						height: 10px;
+						height: 6px;
 						overflow: hidden;
+						background-color: #ddd;
+						border-radius: 3px;
 						span{
 							display: inline-block;
 							height: 100%;
 							width: 24px;
 							background-color: red;
 						}
+					}
+					.red-btn{
+						font-size: 13px;
 					}
 				}
 			}
